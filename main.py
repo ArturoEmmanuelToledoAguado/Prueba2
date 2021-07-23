09ebe896d4f07ada119bcc0a7a7ecfbdfbe35f1a
itzelgarduno <puglove.susy@gmail.com>
2021-07-22 19:17:28 -0500

    arregle un desma y puse la funcion factorial

--- main.py
+++ main.py
@@ -3,33 +3,22 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    suma()
+import math
 
 def suma():
     a = 25
     b = 30
     print(a+b)
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    mult()
-
 def mult():
     a = 88
     b = 11
     print(a*b)
-
+def facto():
+    a = 4
+    b = math.factorial(a)
+    print(b)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    suma()
+    mult()
+    facto()
